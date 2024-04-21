52428ca7fd615b7d5b2b9f98c3b853f8c2b26209
simratdeliverable <simratpreet@deliverable.services>
2024-04-21 14:42:19 +0530

    button text is changed

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,10 +1,17 @@
 import { useNavigate } from "react-router";
 
 const Home = () => {
-    const navigate = useNavigate();
-    return ( <div>
-        <button className="btn btn-primary" onClick={()=>navigate("productList")}>Explore Products</button>
-    </div> );
-}
- 
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button
+        className="btn btn-primary"
+        onClick={() => navigate("productList")}
+      >
+        Find Products
+      </button>
+    </div>
+  );
+};
+
 export default Home;
